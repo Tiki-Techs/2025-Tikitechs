c2c4ba328e38c176d719f0be0a8779d1fb49123c
Andries F <>
2025-03-03 21:30:43 -1000

    actual monday end of session

--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -185,152 +185,152 @@ public class GroundIntake extends SubsystemBase{
         wait = false;
     }
 
-    @Override
-    public void periodic(){
+    // @Override
+    // public void periodic(){
     
-        // double tempsoe = m_hood.get();
-        // if (tempsoe > 1) {
-        //     tempsoe = 1;
-        // }
-        // else if (tempsoe < -1) {
-        //     tempsoe = -1;
-        // }
-        // SmartDashboard.putNumber("Hood Power", tempsoe);
+    //     // double tempsoe = m_hood.get();
+    //     // if (tempsoe > 1) {
+    //     //     tempsoe = 1;
+    //     // }
+    //     // else if (tempsoe < -1) {
+    //     //     tempsoe = -1;
+    //     // }
+    //     // SmartDashboard.putNumber("Hood Power", tempsoe);
 
 
-        SmartDashboard.putNumber("Power Power", m_power.get());
-        realEncoderValue = encoder.get();
-        current = m_power.getOutputCurrent();
+    //     SmartDashboard.putNumber("Power Power", m_power.get());
+    //     realEncoderValue = encoder.get();
+    //     current = m_power.getOutputCurrent();
 
-        if (realEncoderValue > groundIntakeHas || (RobotContainer.m_elevator.setpoint < RobotContainer.m_elevator.groundIntakeLevel || RobotContainer.m_elevator.encoderValue < RobotContainer.m_elevator.groundIntakeLevel)) {
-            canUp = 0;
-        }
-        else {
-            canUp = 1;
-        }
-        if (realEncoderValue < intake) {
-            canDown = 0;
-        }
-        else {
-            canDown = 1;
-        }
+    //     if (realEncoderValue > groundIntakeHas || (RobotContainer.m_elevator.setpoint < RobotContainer.m_elevator.groundIntakeLevel || RobotContainer.m_elevator.encoderValue < RobotContainer.m_elevator.groundIntakeLevel)) {
+    //         canUp = 0;
+    //     }
+    //     else {
+    //         canUp = 1;
+    //     }
+    //     if (realEncoderValue < intake) {
+    //         canDown = 0;
+    //     }
+    //     else {
+    //         canDown = 1;
+    //     }
 
-        if (current > 80
-        //  && MathUtil.applyDeadband(RobotContainer.m_mechController.getLeftTriggerAxis(), 0.15) == 0
-         ) {
-            // have();
-            waiting();
-            have = true;
-        }
+    //     if (current > 80
+    //     //  && MathUtil.applyDeadband(RobotContainer.m_mechController.getLeftTriggerAxis(), 0.15) == 0
+    //      ) {
+    //         // have();
+    //         waiting();
+    //         have = true;
+    //     }
         
-        if(!Intake.have && !have){
-            intake2();
-            // m_power.set(0.1);
-            SmartDashboard.putString("Ground Intake Action", "intake2test");
-        }
-        else if (have && !Intake.have && !RobotContainer.m_mechController.rightBumper().getAsBoolean()
-        //  && (RobotContainer.m_elevator.setpoint > RobotContainer.m_elevator.groundIntakeLevel || RobotContainer.m_elevator.encoderValue > RobotContainer.m_elevator.groundIntakeLevel)
-        ){
-            groundIntakeHas();
-            // m_power.set(0.2);
-            SmartDashboard.putString("Ground Intake Action", "give to intake");
-        }
-        // else if (have) {
-        //     // do a thingy..?
-        //     SmartDashboard.putString("Ground Intake Action", "wait to give to intake");
-        // }
-        else {
-            // m_hood.set(pid.calculate(realEncoderValue, 0)); // change setpoint
-            mainIntakeHas();
-            // m_power.set(0);
-            SmartDashboard.putString("Ground Intake Action", "wait to score");
-        }
+    //     if(!Intake.have && !have){
+    //         intake2();
+    //         // m_power.set(0.1);
+    //         SmartDashboard.putString("Ground Intake Action", "intake2test");
+    //     }
+    //     else if (have && !Intake.have && !RobotContainer.m_mechController.rightBumper().getAsBoolean()
+    //     //  && (RobotContainer.m_elevator.setpoint > RobotContainer.m_elevator.groundIntakeLevel || RobotContainer.m_elevator.encoderValue > RobotContainer.m_elevator.groundIntakeLevel)
+    //     ){
+    //         groundIntakeHas();
+    //         // m_power.set(0.2);
+    //         SmartDashboard.putString("Ground Intake Action", "give to intake");
+    //     }
+    //     // else if (have) {
+    //     //     // do a thingy..?
+    //     //     SmartDashboard.putString("Ground Intake Action", "wait to give to intake");
+    //     // }
+    //     else {
+    //         // m_hood.set(pid.calculate(realEncoderValue, 0)); // change setpoint
+    //         mainIntakeHas();
+    //         // m_power.set(0);
+    //         SmartDashboard.putString("Ground Intake Action", "wait to score");
+    //     }
 
-        if (RobotContainer.m_mechController.rightBumper().getAsBoolean()){
-            // m_Leader.set(Math.signum(RobotContainer.m_driverController.getLeftY()));
-            m_power.set(-0.50);
-            // SmartDashboard.putNumber("right", RobotContainer.m_mechController.getRightTriggerAxis());
-            have = false;
-            // if (have) {
-            //     have = false;
-            // }
-        }
-        else if (RobotContainer.m_mechController.leftBumper().getAsBoolean() && !have){
-            m_power.set(0);
-        }
-        // else if (RobotContainer.m_mechController.x().getAsBoolean() && !have){
-        //     // m_Leader.set(Math.signum(RobotContainer.m_driverController.getLeftY()));
-        //     m_power.set(0.45);
-        //     // SmartDashboard.putNumber("left", RobotContainer.m_mechController.getLeftTriggerAxis());
-        // }
+    //     if (RobotContainer.m_mechController.rightBumper().getAsBoolean()){
+    //         // m_Leader.set(Math.signum(RobotContainer.m_driverController.getLeftY()));
+    //         m_power.set(-0.50);
+    //         // SmartDashboard.putNumber("right", RobotContainer.m_mechController.getRightTriggerAxis());
+    //         have = false;
+    //         // if (have) {
+    //         //     have = false;
+    //         // }
+    //     }
+    //     else if (RobotContainer.m_mechController.leftBumper().getAsBoolean() && !have){
+    //         m_power.set(0);
+    //     }
+    //     // else if (RobotContainer.m_mechController.x().getAsBoolean() && !have){
+    //     //     // m_Leader.set(Math.signum(RobotContainer.m_driverController.getLeftY()));
+    //     //     m_power.set(0.45);
+    //     //     // SmartDashboard.putNumber("left", RobotContainer.m_mechController.getLeftTriggerAxis());
+    //     // }
         
-        // else if (RobotContainer.m_mechController.a().getAsBoolean()){
-        //     m_power.set(0.25);
-        // }
+    //     // else if (RobotContainer.m_mechController.a().getAsBoolean()){
+    //     //     m_power.set(0.25);
+    //     // }
         
-        // else if (RobotContainer.m_mechController.y().getAsBoolean()){
-        //     m_power.set(0.35);
-        // }
-        // else if (RobotContainer.m_mechController.b().getAsBoolean() && !have){
-        //     // m_Leader.set(Math.signum(RobotContainer.m_driverController.getLeftY()));
-        //     m_power.set(0.55);
-        //     // SmartDashboard.putNumber("left", RobotContainer.m_mechController.getLeftTriggerAxis());
-        // }
-        // else {
-        //     if (!wait) {
-        //         m_power.set(0);
-        //         }
-        // }
-        // if (RobotContainer.m_mechController.povUp().getAsBoolean()){
-        //     m_hood.set(0.17);
+    //     // else if (RobotContainer.m_mechController.y().getAsBoolean()){
+    //     //     m_power.set(0.35);
+    //     // }
+    //     // else if (RobotContainer.m_mechController.b().getAsBoolean() && !have){
+    //     //     // m_Leader.set(Math.signum(RobotContainer.m_driverController.getLeftY()));
+    //     //     m_power.set(0.55);
+    //     //     // SmartDashboard.putNumber("left", RobotContainer.m_mechController.getLeftTriggerAxis());
+    //     // }
+    //     // else {
+    //     //     if (!wait) {
+    //     //         m_power.set(0);
+    //     //         }
+    //     // }
+    //     // if (RobotContainer.m_mechController.povUp().getAsBoolean()){
+    //     //     m_hood.set(0.17);
 
 
-        //     // desiredEncoderValue = realEncoderValue;
+    //     //     // desiredEncoderValue = realEncoderValue;
 
 
-        // }
-        // else if (RobotContainer.m_mechController.povDown().getAsBoolean()){
-        //     m_hood.set(-0.17);
+    //     // }
+    //     // else if (RobotContainer.m_mechController.povDown().getAsBoolean()){
+    //     //     m_hood.set(-0.17);
 
 
-        //     // desiredEncoderValue = realEncoderValue;
+    //     //     // desiredEncoderValue = realEncoderValue;
 
 
-        // }
-        // else {
-        //     m_hood.set(0);
+    //     // }
+    //     // else {
+    //     //     m_hood.set(0);
 
 
-        //     // pid command
+    //     //     // pid command
 
 
-        // }
+    //     // }
 
-        // if (MathUtil.applyDeadband(RobotContainer.m_driverController.getRightTriggerAxis(), 0.15) != 0){
-        //     m_power.set(-RobotContainer.m_driverController.getRightTriggerAxis()*0.45);
-        //     have = false;
-        // }
-        // else if (MathUtil.applyDeadband(RobotContainer.m_driverController.getLeftTriggerAxis(), 0.15) != 0 && !have && !wait){
-        //     m_power.set(RobotContainer.m_driverController.getLeftTriggerAxis()*0.35);
-        // }
-        // else {
-        //     if (!wait) {
-        //         m_power.set(0);
-        //         }
-        // }
-        // if (MathUtil.applyDeadband(RobotContainer.m_driverController.getLeftY(), 0.15) != 0) {
-        //     m_hood.set(-RobotContainer.m_driverController.getLeftY()*0.1);
-        // }
-        // else {
-        //     m_hood.set(0);
-        // }
+    //     // if (MathUtil.applyDeadband(RobotContainer.m_driverController.getRightTriggerAxis(), 0.15) != 0){
+    //     //     m_power.set(-RobotContainer.m_driverController.getRightTriggerAxis()*0.45);
+    //     //     have = false;
+    //     // }
+    //     // else if (MathUtil.applyDeadband(RobotContainer.m_driverController.getLeftTriggerAxis(), 0.15) != 0 && !have && !wait){
+    //     //     m_power.set(RobotContainer.m_driverController.getLeftTriggerAxis()*0.35);
+    //     // }
+    //     // else {
+    //     //     if (!wait) {
+    //     //         m_power.set(0);
+    //     //         }
+    //     // }
+    //     // if (MathUtil.applyDeadband(RobotContainer.m_driverController.getLeftY(), 0.15) != 0) {
+    //     //     m_hood.set(-RobotContainer.m_driverController.getLeftY()*0.1);
+    //     // }
+    //     // else {
+    //     //     m_hood.set(0);
+    //     // }
 
 
-        SmartDashboard.putBoolean("have ground", have);
-        // SmartDashboard.putNumber("voltage", m_power.getBusVoltage());
-        SmartDashboard.putNumber("current", m_power.getOutputCurrent());
-        SmartDashboard.putNumber("groundIntake through encoder", realEncoderValue);
-    }
+    //     SmartDashboard.putBoolean("have ground", have);
+    //     // SmartDashboard.putNumber("voltage", m_power.getBusVoltage());
+    //     SmartDashboard.putNumber("current", m_power.getOutputCurrent());
+    //     SmartDashboard.putNumber("groundIntake through encoder", realEncoderValue);
+    // }
 
        
 }
